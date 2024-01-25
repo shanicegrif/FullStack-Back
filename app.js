e063db93a968fc7d2e1e9a20b2e7ed81a450b3a5
dreamseekerfromn <sy@Sungs-Air.fios-router.home>
2024-01-24 22:56:06 -0500

    adds some routes, controllers

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const bookings = require('./controllers/bookingsController.js');
+const meetingRooms = require('./controllers/meetingRoomsController.js')
 const cors = require("cors");
 const app = express();
 
@@ -9,6 +11,9 @@ app.use(express.json());
 app.get("/", (request, response) => {
     response.send("Hello World!");
 });
+
+app.use("/bookings", bookings);
+app.use("/meetingRooms", meetingRooms);
 
 // 404 Page not found
 app.get("*", (req, res) => {
---
+++ controllers/meetingRoomsController.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const {
+    getAllMeetingRooms,
+    getOneMeetingRoom,
+    createMeetingRoom,
+} = require("../queries/meetingRooms");
+
+const meetingRooms = express.Router();
+
+/** get */
+meetingRooms.get("/", async (req, res) => {
+    const allMeetingRooms = await getAllMeetingRooms();
+    
+    if(messages[0]){
+        //no query, show everything
+        res.status(200).json({ success: true, data: { payload: [...allMeetingRooms] } });
+    }
+    else{
+        //do something for queries
+        res.status(400).json({ success: false, data: { error: "Server Error - we didn't do it!" } });
+    }
+});
+
+meetingRooms.get("/:id", async (req, res) => {
+    const { id } = req.params;
+    const oneMeetingRoom = await getOneMeetingRoom(id);
+    
+    if(oneMeetingRoom){
+        //no query, show everything
+        res.status(200).json({ success: true, data: { payload: oneMeetingRoom } });
+    }
+    else{
+        //do something for queries
+        res.status(404).json({ success: false, data: { error: "Server Error - we didn't do it!" } });
+    }
+});
+
+/** post */
+meetingRooms.post("/", async (req, res) => {
+    try{
+        const oneMeetingRoom = await createMeetingRoom(req.body);        
+    } catch(error) {
+        res.status(400).json({error: "something missing in your header"});
+    }
+});
+
+/** page 404 */
+meetingRooms.get("*", (req, res) => {
+    res.status(404).send("with incorrect id - sets status to 404 and returns error key");
+});
+
+module.exports = meetingRooms;
