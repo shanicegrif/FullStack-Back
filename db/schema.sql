54b1be34a2efa461b9fd756fdd43a87d19a86253
Shanice <shanicegriffin@pursuit.org>
2024-01-26 16:31:09 -0500

    fixed schema and create seed

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,20 @@
 DROP DATABASE IF EXISTS booking_db_dev;
 CREATE DATABASE booking_db_dev;
 
-\c thread_db_dev;
+\c booking_db_dev;
+
+
+/*
+    "name": "Meeting Room 1",
+    "capacity": 3,
+    "floor": 22
+*/
+CREATE TABLE meeting_rooms (
+    room_id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    capacity INTEGER NOT NULL,
+    floor INTEGER NOT NULL
+);
 
 /*
 {
@@ -14,22 +27,11 @@ CREATE DATABASE booking_db_dev;
 */
 CREATE TABLE bookings (
     booking_id SERIAL PRIMARY KEY,
-    meetingName VARCHAR(255) UNIQUE NOT NULL,
-    meetingRoomId INTEGER REFERENCES meetingRooms (room_id) ON DELETE CASCADE,
+    meeting_name VARCHAR(255) UNIQUE NOT NULL,
+    meeting_room_id INTEGER REFERENCES meeting_rooms (room_id) ON DELETE CASCADE,
     startDate TIMESTAMP NOT NULL,
     endDate TIMESTAMP NOT NULL,
     attendees TEXT
 );
 
-/*
-    "name": "Meeting Room 1",
-    "capacity": 3,
-    "floor": 22
-*/
-CREATE TABLE meetingRooms (
-    room_id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    capacity INTEGER NOT NULL,
-    floor INTEGER NOT NULL
-);
 
---
+++ db/seed.sql
@@ -0,0 +1,22 @@
+\c booking_db_dev;
+
+/*sample data into the meeting_rooms table*/
+INSERT INTO meeting_rooms (name, capacity, floor) VALUES
+    ('Room A', 10, 1),
+    ('Room B', 8, 2),
+    ('Room C', 12, 3),
+    ('Room D', 6, 4),
+    ('Room E', 15, 5);
+
+/* Insert sample data into the bookings table*/
+INSERT INTO bookings (meeting_name, meeting_room_id, start_date, end_date, attendees) VALUES
+    ('Meeting 1', 1, '2022-03-23T10:00:00.000Z', '2022-03-23T11:30:00.000Z', 'sam@email.com, amy@email.com'),
+    ('Meeting 2', 2, '2022-03-23T14:00:00.000Z', '2022-03-23T15:30:00.000Z', 'dani@email.com, remy@email.com'),
+    ('Meeting 3', 3, '2022-03-24T09:30:00.000Z', '2022-03-24T11:00:00.000Z', 'lucy@email.com'),
+    ('Meeting 4', 4, '2022-03-24T13:00:00.000Z', '2022-03-24T14:30:00.000Z', 'boon@email.com'),
+    ('Meeting 5', 5, '2022-03-25T11:00:00.000Z', '2022-03-25T12:30:00.000Z', 'dean@email.com, maya@email.com'),
+    ('Meeting 6', 1, '2022-03-25T15:00:00.000Z', '2022-03-25T16:30:00.000Z', 'kevin@email.com'),
+    ('Meeting 7', 2, '2022-03-26T10:30:00.000Z', '2022-03-26T12:00:00.000Z', 'tyler@email.com, susan@email.com'),
+    ('Meeting 8', 3, '2022-03-26T14:30:00.000Z', '2022-03-26T16:00:00.000Z', 'mike@email.com'),
+    ('Meeting 9', 4, '2022-03-27T09:00:00.000Z', '2022-03-27T10:30:00.000Z', 'sean@email.com, max@email.com'),
+    ('Meeting 10', 5, '2022-03-27T13:30:00.000Z', '2022-03-27T15:00:00.000Z', 'andy@email.com');
